4051c375457d1dea9571d2a8c4aa967e334ec99f
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-12-30 19:43:21 +0000

    Using clojure.tools.cli

--- src/rdfsplit/core.clj
+++ src/rdfsplit/core.clj
@@ -1,7 +1,40 @@
 (ns rdfsplit.core
+  (:require
+    [clojure.string :as string]
+    [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
+
+
+(def cli-options
+  [["-o" "--output OUTPUTDIR" "Output directory"
+    :default "."]
+  ["-h" "--help"]])
+
+(defn usage [options-summary]
+  (->> ["Split RDF files into reasonably sized N-Quads files."
+        ""
+        "Usage: rdfsplit [options] FILE .."
+        ""
+        "Options:"
+        options-summary
+        ""]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options) (exit 0 (usage summary))
+      (not= (count arguments) 1) (exit 1 (usage summary))
+      errors (exit 1 (error-msg errors)))
+  (parse-opts args cli-options)))
