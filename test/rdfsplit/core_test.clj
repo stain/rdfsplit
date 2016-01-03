1901a88bfe6ce25f89f7a184446438fb28111253
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2016-01-03 17:33:19 +0000

    test in /tmp

--- test/rdfsplit/core_test.clj
+++ test/rdfsplit/core_test.clj
@@ -3,7 +3,7 @@
             [rdfsplit.core :refer :all]))
 
 (deftest a-test
-  
+
   (testing "Splitting no files"
-    (rdfsplit [] {:output tmp})
+    (rdfsplit [] {:output "/tmp"})
     (is (= 0 1))))
